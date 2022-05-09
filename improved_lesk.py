86d3c4a702c9bbc66c2571892cd5712e1fce6974
Andrei Manea <andecenu@gmail.com>
2022-05-09 22:59:12 +0300

    added changes on lesk based on the new best relations

--- improved_lesk.py
+++ improved_lesk.py
@@ -13,6 +13,7 @@ import nltk
 # nltk.download('wordnet_ic')
 STOPWORDS = set(stopwords.words('english'))
 FORBIDEN = STOPWORDS.union(set(string.punctuation))
+lemmatizer = nltk.stem.WordNetLemmatizer()
 
 def pos_map(pos_):
     first_letter = pos_[0].lower()
@@ -21,8 +22,56 @@ def pos_map(pos_):
     return first_letter
 
 def tokenize_and_clean(sentence):
-    sentence_ = [word for word in word_tokenize(sentence) if word not in FORBIDEN]
+    sentence_ = [lemmatizer.lemmatize(word) for word in word_tokenize(sentence) if word not in FORBIDEN]
     return sentence_
+
+def get_subconcept_ngrams(sub_concepts, max_ngrams):
+    all_ngrams = []
+    for sub_concept in sub_concepts:
+        sent = sub_concept.name() + ' ' + sub_concept.definition()
+        sent_clean = tokenize_and_clean(sent)
+        all_ngrams += nltk.everygrams(sent_clean, 1, max_ngrams)
+    return all_ngrams
+
+@lru_cache(maxsize=None)
+def get_ngrams_by_scope(concept, scope, max_ngrams):
+    if scope == "gloss":
+        sent = concept.name() + ' ' + concept.definition()
+        sent_clean = tokenize_and_clean(sent)
+        ngrams = nltk.everygrams(sent_clean, 1, max_ngrams)
+        return ngrams
+
+    elif scope == "example":
+        all_ngrams = []
+        for example in concept.examples():
+            sent_clean = tokenize_and_clean(example)
+            all_ngrams += nltk.everygrams(sent_clean, 1, max_ngrams)
+        return all_ngrams
+
+    elif scope == "hypo":
+        all_ngrams = []
+        for sub_concept in concept.hyponyms():
+            sent = sub_concept.name() + ' ' + sub_concept.definition()
+            sent_clean = tokenize_and_clean(sent)
+            all_ngrams += nltk.everygrams(sent_clean, 1, max_ngrams)
+        return all_ngrams
+
+    elif scope == "mero":
+        meronyms = \
+            concept.substance_meronyms() + \
+            concept.part_meronyms() + \
+            concept.member_meronyms()
+        return get_subconcept_ngrams(meronyms, max_ngrams)
+
+    elif scope == "also":
+        return get_subconcept_ngrams(concept.also_sees(), max_ngrams)
+
+    elif scope == "attr":
+        return get_subconcept_ngrams(concept.attributes(), max_ngrams)
+
+    elif scope == "hype":
+        return get_subconcept_ngrams(concept.hypernyms(), max_ngrams)
+
 
 @lru_cache(maxsize=None)
 def get_extended_concepts(concept):
@@ -53,11 +102,12 @@ def extract_gloss_ngrams(subconcept, max_ngrams):
         gloss_ngrams += nltk.everygrams(example_set, 1, max_ngrams)
     return set(gloss_ngrams)
 
-def get_overlap_score(subconcept_i, subconcept_j, max_ngrams=1):
-    gloss_i = extract_gloss_ngrams(subconcept_i, max_ngrams)
-    gloss_j = extract_gloss_ngrams(subconcept_j, max_ngrams)
+def get_overlap_score(concept_i, concept_j, relation, max_ngrams=1):
+    scope_i, scope_j = relation
+    ngrams_i = get_ngrams_by_scope(concept_i, scope_i, max_ngrams)
+    ngrams_j = get_ngrams_by_scope(concept_j, scope_j, max_ngrams)
 
-    ngrams_intersection = gloss_i & gloss_j
+    ngrams_intersection = set(ngrams_i) & set(ngrams_j)
     values, counts = np.unique(
         list(map(lambda ngram: len(ngram), ngrams_intersection))
     , return_counts=True)
@@ -67,17 +117,60 @@ def get_overlap_score(subconcept_i, subconcept_j, max_ngrams=1):
         score += count ** value
     return score
 
-@lru_cache(maxsize=None)
-def lesk_distance_ant(concept_i, concept_j):
-    distance = 0
+# @lru_cache(maxsize=None)
+# def lesk_distance_ant(concept_i, concept_j):
+#     distance = 0
 
-    concept_i_ext = get_extended_concepts(concept_i)
-    concept_j_ext = get_extended_concepts(concept_j)
+#     concept_i_ext = get_extended_concepts(concept_i)
+#     concept_j_ext = get_extended_concepts(concept_j)
 
-    return sum([concept_j_ext.count(element) for element in concept_i_ext])
+#     return sum([concept_j_ext.count(element) for element in concept_i_ext])
+
+def get_pos_best_relations(pos_i, pos_j):
+    if pos_i == 'n' and pos_j == 'n':
+        return [
+            ("hypo", "mero"),
+            ("mero", "hypo"),
+            ("gloss", "mero"),
+            ("mero", "gloss"),
+            ("gloss", "gloss"),
+            ("example", "mero"),
+            ("mero", "example")
+        ]
+    elif pos_i == 'a' and pos_j == 'a':
+        return [
+            ("also", "gloss"),
+            ("gloss", "also"),
+            ("gloss", "gloss"),
+            ("example", "gloss"),
+            ("gloss", "example"),
+            ("hype", "gloss"),
+            ("gloss", "hype")
+        ]
+    elif pos_i == 'v' and pos_j == 'v':
+        return [
+            ("example", "example"),
+            ("example", "hype"),
+            ("hype", "example"),
+            ("hypo", "hypo"),
+            ("gloss", "hypo"),
+            ("hypo", "gloss"),
+            ("example", "gloss"),
+            ("gloss", "example"),
+        ]
+    return []
 
 @lru_cache(maxsize=None)
 def lesk_distance(concept_i, concept_j, max_ngrams):
+    distance = 0
+
+    relations = get_pos_best_relations(concept_i.pos(), concept_j.pos())
+    for relation in relations:
+        distance += get_overlap_score(concept_i, concept_j, relation, max_ngrams)
+    return distance
+
+@lru_cache(maxsize=None)
+def lesk_distance_legacy(concept_i, concept_j, max_ngrams):
     distance = 0
 
     concept_i_ext = get_extended_concepts(concept_i)
@@ -97,7 +190,7 @@ def lesk_distance2(concept_i, concept_j):
 
     for syn1 in wordnet.synsets(concept_i):
         for syn2 in wordnet.synsets(concept_j):
-            distance += lesk_distance_ant(syn1, syn2)
+            distance += lesk_distance(syn1, syn2, max_ngrams=1)
     return distance
 
 def lesk_distance_full(odour_node, odour_ant):
